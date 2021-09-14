1321d6899155280c7fe2de29e9b4087eac73c756
Safia Jotey <73421569+SafiaJotey@users.noreply.github.com>
2021-09-14 15:35:08 +0600

    Add files via upload
    Initial commit

---
+++ location.js
@@ -0,0 +1,10 @@
+//console.log(window.location);
+ reloadPage=()=>{
+    location.reload();
+}
+
+visitPH=()=>{
+    location.assign('https://web.programming-hero.com/web-4/video/web-4-38-3-location-url-parts-query-string-href-hash-assign-reload');
+}
+location.href='https://web.programming-hero.com/web-4/video/web-4-38-3-location-url-parts-query-string-href-hash-assign-reload'
+ console.log(location.href);
